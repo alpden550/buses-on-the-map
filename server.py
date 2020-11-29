47ad329f22e794bb121dfeeab2e665cd41d6849a
alpden550 <alpden550@gmail.com>
2020-11-29 20:16:45 +0300

    feat(server): listen server

--- server.py
+++ server.py
@@ -4,16 +4,17 @@ from trio_websocket import serve_websocket, ConnectionClosed
 
 async def server(request):
     ws = await request.accept()
+
     while True:
         try:
             message = await ws.get_message()
-            await ws.send_message(message)
+            print(message)
         except ConnectionClosed:
             break
 
 
 async def main():
-    await serve_websocket(server, '127.0.0.1', 8000, ssl_context=None)
+    await serve_websocket(server, '127.0.0.1', 8080, ssl_context=None)
 
 
 if __name__ == '__main__':
