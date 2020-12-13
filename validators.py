e087a2e4317466fcb24b8240a439f74324d96bd0
alpden550 <alpden550@gmail.com>
2020-12-13 12:42:12 +0300

    feat(validators): add browser message validation

---
+++ validators.py
@@ -0,0 +1,24 @@
+import json
+
+
+def validate_browser(msg: str) -> dict or None:
+    result = {"errors": [], "msgType": "Errors"}
+    try:
+        message = json.loads(msg)
+    except json.JSONDecodeError:
+        result['errors'].append('Requires valid JSON')
+        return result
+
+    if not list(message.keys()) == ['msgType', 'data'] or not message['msgType'] == 'newBounds':
+        result['errors'].append('Requires msgType and data')
+        return result
+
+    if not list(message['data'].keys()) == ['south_lat', 'north_lat', 'west_lng', 'east_lng']:
+        result['errors'].append('Requires south_lat, north_lat, west_lng, east_lng in the coordinates.')
+        return result
+
+    try:
+        [float(coordinate) for coordinate in message['data'].values()]
+    except ValueError:
+        result['errors'].append("Can't parse coordinate in float.")
+        return result
