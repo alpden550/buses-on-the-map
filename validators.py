03071c0b6ed3a1825194c85f9fabf2ac5bef6d9c
alpden550 <alpden550@gmail.com>
2020-12-13 13:58:35 +0300

    feat(validators): add bus validation

--- validators.py
+++ validators.py
@@ -22,3 +22,30 @@ def validate_browser(msg: str) -> dict:
     except ValueError:
         result['errors'].append("Can't parse coordinate in float.")
         return result
+
+
+def validate_bus(msg: str) -> dict:
+    result = {"errors": [], "msgType": "Errors"}
+    try:
+        message = json.loads(msg)
+    except (json.JSONDecodeError, TypeError):
+        result['errors'].append('Requires valid JSON')
+        return result
+
+    if not len(message.keys()) == 4:
+        result['errors'].append('Requires all fields for bus.')
+        return result
+
+    if 'busId' not in message:
+        result['errors'].append('Requires busId specified.')
+        return result
+
+    if 'lat' not in message or 'lng' not in message:
+        result['errors'].append('Requires coordinates specified.')
+        return result
+
+    try:
+        _, _ = float(message['lat']), float(message['lng'])
+    except ValueError:
+        result['errors'].append("Can't parse coordinate in float.")
+        return result
